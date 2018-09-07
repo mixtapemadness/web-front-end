1c0015d27c73970dc245eece445bf1a8a15aad2d
btomashvili <bekatomashvili@gmail.com>
2018-09-07 13:23:39 +0400

    update testing.js

--- config/testing.js
+++ config/testing.js
@@ -1,6 +1,6 @@
 module.exports = {
-  apiUrl: 'https://apidev.bookingbravo.com/',
-  apiSocketUrl: 'https://wsdev.bookingbravo.com',
-  apiGraphqlUrl: 'https://apidev.bookingbravo.com/graphql',
-  avatarsUrl: 'https://apidev.bookingbravo.com/avatars',
+  apiUrl: 'http://geobot.ge:8001/',
+  apiSocketUrl: 'http://geobot.ge:8002',
+  apiGraphqlUrl: 'http://geobot.ge:8001/graphql',
+  avatarsUrl: 'http://geobot.ge:8001/avatars',
 }

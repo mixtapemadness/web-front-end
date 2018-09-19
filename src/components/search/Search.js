adc84bb1e6f21f1afeb7cbcd364696db70d9b1c1
giorgi <gtsitsvidze97@gmail.com>
2018-09-19 18:49:54 +0400

    search result

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,9 +1,12 @@
 /* eslint operator-linebreak: 0 */
+/* eslint object-curly-newline: 0 */
 
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-import MobileComponent from 'components/mobileComponent'
+import ClockIcon from 'resources/assets/svgComponents/Clock'
+
 import searchEnhancer from './searchEnhancer'
 import SearchMedia from './searchMedia'
 
@@ -12,9 +15,8 @@ const Container = styled.div`
   top: 0;
   width: 100%;
   height: 100%;
-  background-color: #f7941d;
+  background-color: rgb(247, 148, 29, 0.89);
   z-index: 3;
-  opacity: 0.89;
   overflow-y: auto;
   padding-bottom: 40px;
 `
@@ -69,10 +71,10 @@ const Close = styled.span`
   color: #ffffff;
 `
 const DataContainer = styled.div`
-  max-width: 1200px;
-  margin: 20px auto;
+  width: 100%;
+  margin: 40px auto;
 `
-const ShowMore = styled.div`
+const ShowMore = styled(Link)`
   color: #ffffff;
   font-weight: 600;
   font-size: 17px;
@@ -81,26 +83,91 @@ const ShowMore = styled.div`
   justify-content: center;
   cursor: pointer;
 `
+const Span = styled.div`
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 17px;
+  text-align: center;
+`
+// ========================================================================================================//
 
-const Search = ({ toggleSearch, handleSubmit, data }) => {
-  const searchResut =
+const ItemContainer = styled.div`
+  width: 77%;
+  max-width: 1028px;
+  margin: auto;
+`
+
+const Item = styled.div`
+  width: 50%;
+  padding-bottom: 20px;
+  margin-top: 20px;
+  border-bottom: 1px solid #ccc;
+  display: flex;
+`
+const ContentContainer = styled.div`
+  width: 50%;
+  margin-left: 10px;
+  font-size: 16px;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: #ffffff;
+`
+const Time = styled.span`
+  margin-left: 10px;
+`
+
+const TimeContainer = styled.div`
+  display: flex;
+  font-size: 13px;
+`
+
+const getDate = date => {
+  const newDate = new Date(date)
+  return `${newDate.getDay()}/${newDate.getMonth()}/${newDate.getFullYear()}`
+}
+
+const renderData = searchResult => (
+  <DataContainer>
+    {searchResult && searchResult.length > 0
+      ? searchResult.map(item => (
+          <ItemContainer key={item.id}>
+            <Item>
+              <SearchMedia id={item.featured_media} />
+              <ContentContainer>
+                <span>{item.title}</span>
+                <TimeContainer>
+                  <ClockIcon height="20px" color="#ffffff" />
+                  <Time>{getDate(item.date)}</Time>
+                </TimeContainer>
+              </ContentContainer>
+            </Item>
+          </ItemContainer>
+        ))
+      : ''}
+  </DataContainer>
+)
+
+const Search = ({ toggleSearch, handleSubmit, data, value }) => {
+  const searchResult =
     data && data.searchedData && data.searchedData.length > 0
       ? data.searchedData
-      : {}
+      : null
   return (
     <Container>
-      <SearchMedia id={searchResut.featured_media} />
-      {console.log('data from search', data)}
       <Close onClick={() => toggleSearch()}>X</Close>
       <InputContainer>
         <Input placeholder="Search" onKeyPress={handleSubmit} />
       </InputContainer>
-      <DataContainer>
-        {searchResut && searchResut.lenght > 0
-          ? searchResut.map(item => <MobileComponent data={item} />)
-          : ''}
-      </DataContainer>
-      {data && <ShowMore>Show more +</ShowMore>}
+      {renderData(searchResult)}
+      {searchResult && (
+        <ShowMore to={`/searchresult/${value}`} onClick={() => toggleSearch()}>
+          View all
+        </ShowMore>
+      )}
+      {!searchResult &&
+        value !== '' && <Span>No results found for {value}</Span>}
     </Container>
   )
 }

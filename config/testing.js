861b60d3c5c06ebc2ddad54a4cb23d0d1d85a8ac
btomashvili <bekatomashvili@gmail.com>
2018-09-19 15:09:21 +0400

    update test config

--- config/testing.js
+++ config/testing.js
@@ -1,6 +1,6 @@
 module.exports = {
-  apiUrl: 'http://geobot.ge:8001/',
-  apiSocketUrl: 'http://geobot.ge:8002',
-  apiGraphqlUrl: 'http://geobot.ge:8001/graphql',
-  avatarsUrl: 'http://geobot.ge:8001/avatars',
+  apiUrl: 'http://mixtapeapi.vobi.io/',
+  apiSocketUrl: 'http://mixtapeapi.vobi.io:8006',
+  apiGraphqlUrl: 'http://mixtapeapi.vobi.io/graphql',
+  avatarsUrl: 'http://mixtapeapi.vobi.io/avatars',
 }

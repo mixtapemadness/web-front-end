7d80fb6bc334431d2c57429340a333018ee4c866
btomashvili <bekatomashvili@gmail.com>
2019-01-11 16:19:03 +0400

    update production config

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
-  apiUrl: 'http://mixtapeapi.vobi.io/',
-  apiSocketUrl: 'http://mixtapeapi.vobi.io:8006',
-  apiGraphqlUrl: 'http://mixtapeapi.vobi.io/graphql',
-  avatarsUrl: 'http://mixtapeapi.vobi.io/avatars',
+  apiUrl: 'http://blog.mixtapemadness.com:8001',
+  apiSocketUrl: 'http://blog.mixtapemadness.com:8006',
+  apiGraphqlUrl: 'http://blog.mixtapemadness.com:8001/graphql',
+  avatarsUrl: 'http://blog.mixtapemadness.com:8001/graphql/avatars',
 }

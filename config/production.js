daea5c4660ba17ba51b321f0a9592c852d6688ed
kkbokyere <kkbokyere@gmail.com>
2019-01-21 14:43:22 +0000

    - update https

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
-  apiUrl: '/blog.mixtapemadness.com:8001',
-  apiSocketUrl: '/blog.mixtapemadness.com:8006',
-  apiGraphqlUrl: 'blog.mixtapemadness.com:8001/graphql',
-  avatarsUrl: '/blog.mixtapemadness.com:8001/graphql/avatars',
+  apiUrl: 'https://blog.mixtapemadness.com:8001',
+  apiSocketUrl: 'https://blog.mixtapemadness.com:8006',
+  apiGraphqlUrl: 'https://blog.mixtapemadness.com:8001/graphql',
+  avatarsUrl: 'https://blog.mixtapemadness.com:8001/graphql/avatars',
 };

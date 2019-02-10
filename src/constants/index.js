4d436434fe06cdeb95c94ce2aea4931aa2aff679
kkbokyere <kkbokyere@gmail.com>
2019-02-10 22:17:28 +0000

    update favicon

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ export const RESPONSIVE_BREAKPOINTS = {
   desktop: '1024px',
 };
 
-export const IMAGES_URL = 'http://images.mixtapemadness.co.uk';
+export const IMAGES_URL = 'https://images.mixtapemadness.co.uk';
 
 export const ROUTES = {
   base: 'https://www.mixtapemadness.com',

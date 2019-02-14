81225f4e0a230297d06889703bd5c279e1c7eecd
kkbokyere <kkbokyere@gmail.com>
2019-02-14 02:04:37 +0000

    used share this instead of add this

--- server/index.js
+++ server/index.js
@@ -118,7 +118,7 @@ app.get('*', (req, res) => {
           }}
         />
         <script src="/bundle.js" charSet="UTF-8" async />
-        <script src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5c0e4bff29290756" async />
+        <script async src="//platform-api.sharethis.com/js/sharethis.js#property=5c64bf387056550011c4a0bc&product=inline-share-buttons" />
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
         <script dangerouslySetInnerHTML={{
           __html: `${JSON.stringify(

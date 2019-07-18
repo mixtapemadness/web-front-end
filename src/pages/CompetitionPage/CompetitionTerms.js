6e0ca0f63035e3d67b67066b741fc450f2fdf523
kingsleyokyere <kkbokyere@gmail.com>
2019-07-18 23:20:25 +0100

    - added terms and conditions

--- src/pages/CompetitionPage/CompetitionTerms.js
+++ src/pages/CompetitionPage/CompetitionTerms.js
@@ -3,22 +3,108 @@ import React from 'react';
 import styled from 'styled-components';
 import Page from '../Page';
 
-const Heading = styled.h1`
-  font-weight: 800;
-  text-align: left;
-  margin-bottom: 15px;
+const Indent = styled.div`
+  margin-left: 15px;
 `;
 
 const CompetitionTerms = () => (
   <Page>
-    <Heading>FIFA eWorld Cup Terms & Conditions</Heading>
+    <h2>Rules of the FIFA Head to Head Freestyle Contest (the "Contest")</h2>
     <p>
-      These terms and conditions outline the rules and regulations for the use
-      of Mixtape Madness's Website. By accessing this website, we assume you
-      accept these terms and conditions in full. Do not continue to use Mixtape
-      Madness's website if you do not accept all of the terms and conditions
-      stated on this page.
+      Organised by:<br />
+      Fédération Internationale de Football Association<br />
+      FIFA Strasse 20<br />
+      P.O. Box<br />
+      8044 Zürich<br />
+      Switzerland<br />
+      ("<strong>FIFA</strong>")
     </p>
+    <p>1. <u>Eligibility</u>. The Contest is only open to participants who are:</p>
+    <Indent>
+      <p>(a) residents of the United Kingdom; and</p>
+      <p>(b) at least sixteen (16) years of age as at the date of their entry into the Contest.</p>
+      <p>(c) Directors, officers, employees, contractors and agents of FIFA, including its subsidiaries, and members of any of their immediate families (i.e. spouses/partners, parents, siblings, grandparents, grandchildren or children) are not eligible to enter the Contest. Winning the prize is contingent upon fulfilling all requirements set forth herein. Participation in the Contest does not create a contractual relationship with FIFA.</p>
+    </Indent>
+    <p><u>Timings</u>. The Contest will commence from 09:00 (British Summer Time ("<b>BST</b>") on 19 July 2019. The Contest will close at 12:00 BST (midday) on 22 July 2019 the <b>Closing Date</b>"). FIFA reserves the right to exclude any entries received after the Closing Date.</p>
+    <p>2. <u>Participation</u>. In order to be able to participate in the Contest, you are required to:</p>
+    <Indent>
+      <p>(a) ensure that your personal Instagram network account privacy settings are configured to allow FIFA to review your entry and contact you throughout the period of the Contest;</p>
+      <p>(b) navigate to <a href="/fifa-eworld-cup-competition">mixtapemadness.com/fifa-eworld-cup-competition</a></p>
+      <p>(c) download the music beat therein;</p>
+      <p>(d) create a FIFA or football inspired freestyle music video which will need to conform with the requirements set out in the video requirements section below ("Video"); and</p>
+      <p>(e) upload the Video to your Instagram channel using both the #FIFAFreestyle and #FIFAeWorldCup hashtags before the Competition deadline.</p>
+    </Indent>
+    <p>2.1The Video must:</p>
+    <Indent>
+      <p>(a) use the beat downloaded under 2(a) above;</p>
+      <p>(b) be of a FIFA or football inspired freestyle ("Freestyle"); and</p>
+      <p>(c) not use any other music sample or other sound recording apart from the provided beat.</p>
+    </Indent>
+    <p>2.2 You warrant and undertake that:</p>
+
+    <Indent>
+      <p>(d) the Video and Freestyle shall not show any inappropriate or dangerous behaviour, promote any particular political agenda or message, or be obscene, defamatory, distasteful, derogatory, offensive, or in breach of any applicable laws;</p>
+      <p>(e)the Video and Freestyle shall be your own original work, and does not contain any third-party materials, samples and/or content other than the provided beat;</p>
+      <p>(f) the express permission of all persons featured in the Video has been obtained and does not feature any persons under the age of 16;</p>
+      <p>(g) if you or any participant in the Video are under 18 then you have obtained the permission of a parent or guardian to participate;</p>
+      <p>(h) you are not signed to any exclusive recording agreement and you have the right and capacity to enter into this agreement and grant FIFA the rights set out herein;</p>
+      <p>(i) the Video and Freestyle do not promote your own or any third-party products, services or brands or otherwise contain your own or third-party branding;</p>
+      <p>(j) you waive and/or irrevocably agree not to assert any and all moral rights you may have in the Video and Freestyle.</p>
+    </Indent>
+    <p>(together, the "<strong>Warranties</strong>")</p>
+
+    <p>At any point during the Contest, FIFA can request verification of your eligibility to enter the Competition and may disqualify you if FIFA consider, in their sole discretion, that you are not eligible or that you have entered the Competition to discredit the Competition or otherwise in bad faith.</p>
+    <p>3. <u>Limitation</u>. All users may only participate in the Contest once. Multiple entries to the Contest shall be deemed void. Entries on behalf of another person shall not be accepted. The use of any automated software or multiple accounts is prohibited.</p>
+    <p>4. <u>Winner and prize.</u> Following the Closing Date, a FIFA jury will select from all the entries to the Contest four (4) winning valid entries. The FIFA jury shall select four (4) winning Videos based on, in its absolute opinion, those which it considers to be the most original and creative ("<strong>Semi-Finalists</strong>").</p>
+    <p>Each of the four (4) Semi Finalists shall:</p>
+    <Indent>
+      <p> - be invited to participate in the Head to Head Freestyle Finals (the "<b>Finals</b>") at the FIFA eWorld Cup Final scheduled to be held in London, United Kingdom on 4 August 2019 (the "<b>Event</b>");</p>
+      <p> - receive transportation from the Semi-Finalist’s residence to a hotel of FIFA’s choosing ("<b>Hotel</b>");</p>
+      <p> - receive one (1) taxi fare from the Hotel to the Event venue;</p>
+      <p> - receive an allowance to cover general expenses in the total value of GBP £50.00, comprising GBP £20.00 to be provided on 25 July 2019 and GBP £30.00 to be provided on 4 August 2019;</p>
+      <p> - be entitled to receive a personalised head to head freestyle Adidas t-shirt; and</p>
+      <p> - receive one (1) night of accommodation (if required) for the live event on 4th August in London, United Kingdom at the Hotel.</p>
+    </Indent>
+    <p>In order to participate in the Event, you are agreeing to the following (in each case at a time and place determined by FIFA in its sole discretion):</p>
+    <Indent>
+      <p> - you shall enter into an artist service agreement with FIFA’s brand agent, Ear to the Ground (UK) Ltd to supply your services for the Event;</p>
+      <p> - attend and take part in a content shoot (photos and videos) scheduled for 25 July 2019 and on the day of the Event;</p>
+      <p> - attend rehearsals / pre-Event briefing for the Finals;</p>
+      <p> - share your experience on social media channels; and</p>
+      <p> - perform in front of a live audience which will also be live streamed as part of the Event, where:</p>
+      <Indent>
+        <p> - all performances will be FIFA or Football inspired freestyles and the Warranties are deemed to be repeated in respect of such freestyles;</p>
+        <p> - the Semi-Finalists will then perform in the final of the Finals and the overall winner shall be determined by a public vote (the "<b>Winner</b>");</p>
+      </Indent>
+    </Indent>
+    <p>The Winner will be awarded the following:</p>
+    <Indent>
+      <p> - the opportunity to perform at the FIFA eWorld Cup after party;</p>
+      <p> - the Winner’s freestyle video shall be hosted on Mixtape Madness; and</p>
+      <p> - the Winner shall have the opportunity to be featured in an episode of Mixtape Madness’ "Next Up" series.</p>
+    </Indent>
+    <p>FIFA’s decision in connection with any aspect of the Contest shall be final, binding and not capable of appeal.</p>
+    <p>For the avoidance of doubt, all aspects of the prize are stated above. To the extent that FIFA ensures that the above prize items are made available to you, FIFA will be deemed to have fulfilled its prize obligations. All costs and expenses not explicitly stated above as comprising part of the prize shall be the sole responsibility of the winner. Such costs and expenses, which will not be covered by FIFA, shall include (without limitation): insurance, visa charges and taxes, travel to and from any airport, spending money (beyond the daily allowance expressly stated under paragraph 4 above), meals and any hotel incidentals (e.g. phone, laundry, mini bar). The winner waives any and all claims (if any) for relief (including restitution) against FIFA and their respective bodies, officers, agents, employees, volunteers, sponsors and any other entity or person acting on their behalf should the purpose of such ancillary expenses be or become in whole or in part frustrated for any reason whatsoever. All relevant insurance are the Winner’s own responsibility.
+    </p>
+    <p>
+      Each winner cannot request a cash alternative or substitution of the prize.  FIFA, at its sole discretion, reserves the right to substitute a prize of greater or equal value in particular if any element of the prize cannot be awarded as described due to unavailability.  Notwithstanding the foregoing, FIFA reserves the right to make, at its sole discretion, changes to the prize.
+    </p>
+    <p>
+      As a condition of claiming a prize, you will be required to sign an Affidavit (hereafter defined in paragraph 5).
+    </p>
+    <p><u>Conditions to claim the prize</u>. Each winner will be notified by FIFA by way of direct message via Instagram. In order to win the prize, each winner will be required to sign and return to FIFA an Affidavit of Eligibility and Authorisation pursuant to which the relevant Winner will, inter alia, confirm their eligibility to participate in the Contest (the "Affidavit"). If a potential winner is legally considered to be a minor, the prize will be awarded to the minor’s parents or legal guardian, who must sign and return the Affidavit on behalf of the potential winner. Please note that the Affidavit will also require the consent of the potential winner (or the potential winner’s parent or legal guardian, as appropriate) to have FIFA use his or her name, photograph, submitted photograph, likeness, voice, prize information and biographical information for publicity and promotional purposes without further compensation.</p>
+  <p>However, if a winner fails to sign and return the requested Affidavit within twenty four (24) hours of being notified that they are a winner;</p>
+    <p>the winner shall be disqualified and shall forfeit the opportunity to claim the prize. Thereafter, FIFA shall, in accordance with the criteria set out in paragraph 4 above, select another winner. Following FIFA’s receipt of the executed Affidavit from the Winner, FIFA will provide the prizes to the winners on-site at the Event.</p>
+    <p>6. <u>Rights in the Videos</u>. FIFA and Mixtape Madness will have the right to use and exploit the Video and Freestyle by all and any means whether now known or not from the date on which you upload the Video to Instagram.</p>
+    <p>Additionally, the Semi-Finalists grant FIFA an exclusive licence for their video and performance at the Event to be live streamed and/or transmitted as part of the Event, or as a separate clip, and for all ongoing promotional use in respect of the Competition and Event, to remain in FIFA’s public facing archive in perpetuity.</p>
+    <p>All rights in the recording of the Event (the program, which shall include all edits thereof), whether including your performance, the Video or otherwise, is the exclusive property of FIFA and FIFA shall be entitled to exploit the Program in perpetuity without reference or payment to you.
+    </p>
+    <p>7. <u>Cancellation</u>. FIFA reserves the right, in its sole discretion, to cancel, suspend and/or modify the Contest, in particular if any fraud, technical failures or any other factor beyond FIFA’s reasonable control impairs the integrity or proper functioning of the Contest.  FIFA reserves the right, in its sole discretion, to disqualify any user it finds to be tampering with the FIFA contest page, the participation process or the operation of the Contest.
+    </p>
+    <p>8. <u>Disclaimer and Limitation of Liability</u>. FIFA is not responsible for: (1) any incorrect or inaccurate information; (2) technical failures of any kind; (3) unauthorised human intervention in any part of the Contest; (4) any damage, loss, injury, disappointment or psychological stress/trauma suffered by any participant entering the Contest or as a result of acceptance of any prize; (5) participants ensuring that they are legally allowed to participate in the Contest; or (6) the prejudice resulting from any of the events listed under (1) to (5).
+    </p>
+    <p>9. <u>Privacy</u>. Personal information shall be collected by Mixtape Madness Ltd during the Contest and such information will be limited to name, surname, city of residence, telephone number and email address of participants. Such personal data shall be stored in England and shall be shared with FIFA and Ear to the Ground (UK) Ltd for the sole purpose of administering the Contest and to enable Mixtape Madness to contact the potential winner. This data shall be deleted by FIFA, Mixtape Madness and Ear to the Ground (UK) Ltd once the Contest terminates. In case of any questions regarding your personal information, you may contact info@mixtapemadness.com.</p>
+    <p>10. <u>Governing Law</u>. This Agreement is to be governed by, and interpreted in accordance with, the laws of Switzerland, to the exclusion of any choice of law principles and to the exclusion of the Vienna Convention on Contracts for the International Sale of Goods.</p>
   </Page>
 );
 

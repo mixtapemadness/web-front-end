77fea998a4160cc71b97df863a763e96e719ad69
kingsleyokyere <kkbokyere@gmail.com>
2020-05-02 12:22:20 +0100

    - update react router dom - add use location

--- src/components/Navigation/DropDownNavItem.js
+++ src/components/Navigation/DropDownNavItem.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import IconButton from '../IconButton';
 import { RenderNavItems } from './Navigation';
 
-const DropDownNavItem = (props) => {
-  const {
-    location, categories, title, navLinks,
-  } = props;
-  const [showSecondaryNav, setSecondaryNav] = useState(false);
+const DropDownNavItem = ({
+  location, categories, title, navLinks, isOpen,
+}) => {
+  const [showSecondaryNav, setSecondaryNav] = useState(isOpen);
   return (
     <li className={`navigation__item ${showSecondaryNav ? 'navigation__item--is-active' : ''}`} key="second-nav">
       <IconButton iconClassName={`fa ${showSecondaryNav ? 'fa-chevron-up' : 'fa-chevron-down'}`} className="btn-link" onClick={() => setSecondaryNav(!showSecondaryNav)}>{title} </IconButton>
